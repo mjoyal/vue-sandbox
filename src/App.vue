9011635e3d3ba706eeef884a469964a4574e1645
“mjoyal” <mackenzie.joyal@gmail.com>
2023-01-09 17:52:08 -0500

    add Product Card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import NavBar from "./components/NavBar.vue";
+import ProductCard from "./components/ProductCard.vue";
 </script>
 
 <template>
   <NavBar />
+  <div class="main-product">
+    <ProductCard title="Bunny Tee" price="$69.99" :likeCount="420" />
+  </div>
 </template>
 
 <style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  price: string;
+  likeCount: number;
+}>();
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="image"></div>
+
+    <div class="card-bottom">
+      <div>
+        <h5>{{ title }}</h5>
+        <p>{{ price }}</p>
+      </div>
+      <div>
+        <p>❤️ {{ likeCount }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 250px;
+  background-color: beige;
+}
+
+.image {
+  height: 350px;
+  background-color: pink;
+}
+.card-bottom {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: aquamarine;
+}
+</style>
